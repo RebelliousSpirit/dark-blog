2454ed21194e58aa843c69c83cb15a459b743455
RebelliousSpirit <rebelliousspirit111@gmail.com>
2020-01-03 15:56:39 +0600

    Modify the smooth loading of lazyload images.js

---
+++ app/js/_custom.js
@@ -0,0 +1,59 @@
+document.addEventListener("DOMContentLoaded", function() {
+	/**
+	 * disappearing menu
+	 * 
+	 */
+	
+	$('#header .menu-swith').on('click', function(event) {
+		$('#opacity-menu').fadeIn(600).show(600);
+	});
+
+	$('#opacity-menu .menu-swith').on('click', function(event) {
+		
+		$('#opacity-menu').fadeOut(600).hide(600);
+	
+	});
+	
+	/**
+	 * setting up lazy loading of images
+	 * 
+	 */
+	
+	// $('img.lazy').lazyLoad();
+
+	/*
+	* parallax effect in live bg
+	*
+	*/
+
+	function moveLiveImg(event) {
+		
+		let bgItems = document.querySelectorAll('#live-bg .live-bg__item');
+		let coordX = event.clientX;
+		let coordY = event.clientY; 
+
+		bgItems.forEach( function(item) {
+			
+			let itemSpeed = item.getAttribute('data-speed');
+			let translateValueX = (coordX*itemSpeed)/50;
+			let translateValueY = (coordY*itemSpeed)/50;
+
+			item.style.transform = 'translate('+translateValueX+'px, '+translateValueY+'px)';
+
+		});
+		
+	}
+
+	document.addEventListener('mousemove', moveLiveImg);
+
+	/**
+	 * preloader
+	 */	
+	$('#preloader').delay(1000).fadeOut(600);
+
+	/**
+	 * animated title at the top of the page 
+	 */
+
+	
+});
